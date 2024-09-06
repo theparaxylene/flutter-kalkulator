aba020c3578e8356924492ac042c9acd9394bab0
Para Xylene <bigboiparaxylene@gmail.com>
2024-09-06 13:43:38 +0330

    start history-box

---
+++ lib/widgets/history_box.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HistoryBox extends StatelessWidget {
+  const HistoryBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 300,
+      width: double.infinity,
+      child: SingleChildScrollView(
+        child: Column(),
+      ),
+    );
+  }
+}
