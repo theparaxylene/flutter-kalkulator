4fb743b564876a8c425e51b83f3fc19a99f47a74
Para Xylene <bigboiparaxylene@gmail.com>
2024-09-07 16:12:50 +0330

    add and style the title placeholder in history screen

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -13,6 +13,9 @@ class HistoryScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+        ),
         body: ListView.builder(
           itemCount: solvedQuestions.length,
           itemBuilder: (context, index) => Padding(
@@ -20,13 +23,47 @@ class HistoryScreen extends StatelessWidget {
               horizontal: 16,
               vertical: 8,
             ),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                // color: Colors.blueGrey.shade100,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(8),
+                  topRight: Radius.circular(8),
+                ),
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.blueGrey.shade600,
+                    width: 0.75,
+                  ),
+                ),
+              ),
+              child: Column(
                 children: [
-                  Text(
-                    solvedQuestions[index],
-                    style: const TextStyle(fontSize: 32),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'title',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  SizedBox(
+                    width: double.infinity,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            solvedQuestions[index],
+                            style: const TextStyle(fontSize: 32),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
